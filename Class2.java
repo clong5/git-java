67c8ab24040b797032484bbe760fb012a00a7b2d
Chandler Long <clong13@stu.jsu.edu>
2026-09-01 12:03:35 -0500

    v3.1, Completed hotfix

--- Class2.java
+++ Class2.java
@@ -14,7 +14,7 @@ public class Class2 {
     }
 
     private void f3() {
-        // v3.0
+        // v3.1
     }
 
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
     }
 
     private void f3() {
-        // v3.0
+        // v3.1
     }
 
 }
